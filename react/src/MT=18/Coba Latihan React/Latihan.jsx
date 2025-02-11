f9fe8558ba8cff1e75de1dade13d26453cb2aa1c
hmmskuy <160695648+hmmskuy@users.noreply.github.com>
2025-02-11 11:09:16 +0700

    tugas 18 clear
    Update  & Delet from API

---
+++ react/src/MT=18/Coba Latihan React/Latihan.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const UpdateUser = () => {
+  const [userId, setUserID] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    avatar: "",
+  });
+  const [message, setMessage] = useState("");
+
+  // Fungsi untuk menangani perubahan input form
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleupdateUser = (e) => {
+    e.preventDefault();
+
+    // Mengirim data ke API untuk meperbarui pengguna
+    axios
+      .put(`https://api.escuelajs.co/api/v1/users/${userId}`, formData)
+      .then((response) => setMessage("User updated: ${response.data.name}"))
+      .catch((error) => setMessage("Error: updating user:  ${error.message}"));
+
+    // Reset form
+    setUserId("");
+    setFormData({
+      name: "",
+      email: "",
+      avatar: "",
+    });
+  };
+};
