a15edef2da30955dd70123df49d7664bede593cb
Tiiilod <turky2202@student.abudzar.sch.id>
2025-04-30 10:43:08 +0700

    Test Collaborators

--- sas/SAS2425/main.jsx
+++ sas/SAS2425/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+
 import Card from './ujian/Praktek/Racket/prodect'
+import App from './app'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Card/>
+    <App/>
   </StrictMode>,
 )
